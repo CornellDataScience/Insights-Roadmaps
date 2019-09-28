168dcb6be39f248b3a16c7d262df5b37deaae37a
Tushar Khan <tak62@cornell.edu>
2019-09-28 13:58:05 -0400

    Added some dummy code

---
+++ model/node.py
@@ -0,0 +1,6 @@
+
+class Node:
+    """Represents a point in the network where the car is required to stop or make a decision on how to continue"""
+
+    def __init__(self):
+        self.edge_mapping = []
