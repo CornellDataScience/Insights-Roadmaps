e95960df13d058f615228e803f559d4879b32151
Ziwei Gu <zg48@cornell.edu>
2019-10-20 12:59:34 -0400

    create animation

---
+++ animation.py
@@ -0,0 +1,71 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class ParticleBox:
+    def __init__(self,
+                 init_state,
+                 bounds = [-2, 2, -2, 2],
+                 size = 0.04):
+        self.init_state = np.asarray(init_state, dtype=float)
+        self.size = size
+        self.state = self.init_state.copy()
+        self.time_elapsed = 0
+        self.bounds = bounds
+
+    def step(self, dt):
+        self.time_elapsed += dt
+        self.state[:, :2] += dt * self.state[:, 2:]
+
+        crossed_x1 = (self.state[:, 0] < self.bounds[0] + self.size)
+        crossed_x2 = (self.state[:, 0] > self.bounds[1] - self.size)
+        crossed_y1 = (self.state[:, 1] < self.bounds[2] + self.size)
+        crossed_y2 = (self.state[:, 1] > self.bounds[3] - self.size)
+
+        self.state[crossed_x1, 0] = self.bounds[0] + self.size
+        self.state[crossed_x2, 0] = self.bounds[1] - self.size
+
+        self.state[crossed_y1, 1] = self.bounds[2] + self.size
+        self.state[crossed_y2, 1] = self.bounds[3] - self.size
+
+        self.state[crossed_x1 | crossed_x2, 2] *= -1
+        self.state[crossed_y1 | crossed_y2, 3] *= -1
+
+init_state =[[0,1,1,0], [1,1,0,1], [1,1,-1,0],[-1,-1,1,0],[-2,-1,0,1]]
+
+box = ParticleBox(init_state, size=0.04)
+dt = 1. / 50
+#============================================================#
+fig = plt.figure()
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                     xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
+
+particles, = ax.plot([], [], 'bo', ms=6)
+
+rect = plt.Rectangle(box.bounds[::2],
+                     box.bounds[1] - box.bounds[0],
+                     box.bounds[3] - box.bounds[2],
+                     ec='none', lw=2, fc='none')
+ax.add_patch(rect)
+
+def init():
+    global box, rect
+    particles.set_data([], [])
+    rect.set_edgecolor('none')
+    return particles, rect
+
+def animate(i):
+    global box, rect, dt, ax, fig
+    box.step(dt)
+    
+    rect.set_edgecolor('k')
+    particles.set_data(box.state[:, 0], box.state[:, 1])
+    particles.set_markersize(8)
+    return particles, rect
+
+ani = animation.FuncAnimation(fig, animate, frames=600,
+                              interval=10, blit=True, init_func=init)
+
+plt.show()
