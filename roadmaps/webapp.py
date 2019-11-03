b71f09cec0f5f7d1c601fa05f79a6b241dfaae5a
Ruchika Dongre <rd438@cornell.edu>
2019-11-03 16:01:36 -0500

    renaming

--- roadmaps/webapp.py
+++ roadmaps/webapp.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 
 #ogging.basicConfig(level=logging.DEBUG)
-def hello_world():
+def run_app():
     #new_dict={'car1': [{'lat': 42.436483, 'lng': -76.507386}, {'lat': 42.438501, 'lng': -76.464116}, {'lat': 42.447367, 'lng': -76.470143}]}
     temp_planar_graph = generate_planar_graph.getPlanarGraphAsDict()
-    return render_template("index-2.html", cars=temp_planar_graph)
+    return render_template("index.html", cars=temp_planar_graph)
 
 if __name__ == '__main__':
     app.run()
