aef97529fe1f197d17a9b92ad2d1ff852c9d4bb2
TusharK54 <tushar.54k@gmail.com>
2019-11-09 14:02:25 -0500

    Improved test suite

--- roadmaps/tests/test_model_graph.py
+++ roadmaps/tests/test_model_graph.py
@@ -6,6 +6,51 @@ from time import time_ns as now
 
 from model.graph import Edge, Node, Graph
 
+class TestEdge(unittest.TestCase):
+
+    def setUp(self):
+        self.weight = 10
+        self.e = Edge(self.weight)
+
+    def test_traverse(self):
+        self.assertEqual(self.e.traverse(0), self.weight)
+
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        self.n1, self.n2, self.e = Node(), Node(), Edge()
+        self.n1.connect_to(self.n2, self.e)
+
+    def test_traverse(self):
+        self.assertEqual(self.n1.traverse(0), 0)
+        self.assertEqual(self.n2.traverse(0), 0)
+
+    def test_get_neighbors(self):
+        self.assertEqual(self.n1.get_neighbors(), [self.n2])
+        self.assertEqual(self.n2.get_neighbors(), [])
+
+    def test_get_edges(self):
+        self.assertEqual(self.n1.get_edges(), [self.e])
+        self.assertEqual(self.n2.get_edges(), [])
+
+    def test_get_parents(self):
+        self.assertEqual(self.n1.get_parents(), [])
+        self.assertEqual(self.n2.get_parents(), [self.n1])
+    
+    def test_get_input_edges(self):
+        self.assertEqual(self.n1.get_input_edges(), [])
+        self.assertEqual(self.n2.get_input_edges(), [self.e])
+
+    def test_replace_edge(self):
+        new_edge = Edge()
+        self.n1.connect_to(self.n2, new_edge)
+
+        self.assertEqual(self.n1.get_edges(), [new_edge])
+        self.assertEqual(self.n2.get_input_edges(), [new_edge])
+
+        
+
 class TestGraph(unittest.TestCase):
 
     def setUp(self):
@@ -49,12 +94,14 @@ class TestGraph(unittest.TestCase):
         self.assertEqual([], graph.get_nodes())
         self.assertEqual([], graph.get_edges())
 
-        n1, n2, e = Node(), Node(), Edge(1)
+        n1, n2, e = Node(), Node(), Edge()
         graph.connect(n1, n2, e)
-        self.assertEqual(n1.get_neighbors(), [n2])
-        self.assertEqual(n1.get_edges(), [e])
         self.assertEqual(set((n1, n2)), set(graph.get_nodes()))
         self.assertEqual([e], graph.get_edges())
+
+    def test_no_path(self):
+        n1 = Node()
+        self.assertEqual(self.graph.get_path(n1, self.nodes[0]), None)
 
 
 if __name__ == '__main__':
