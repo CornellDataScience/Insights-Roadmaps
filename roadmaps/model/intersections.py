b5e3041f0752a9060ef24bdf4e6e44e68adae50a
Tushar Khan <tak62@cornell.edu>
2019-11-09 12:27:26 -0500

    Extended model components with Traversable class for simulation

--- roadmaps/model/intersections.py
+++ roadmaps/model/intersections.py
@@ -1,27 +1,12 @@
 
-from .graph import Edge
+from .graph import Edge, Node, Graph
 from .network import TrafficEdge, TrafficNode, TrafficNetwork
 
-class Intersection(TrafficNode):
+class Intersection(Node):
 
     def __init__(self):
         super().__init__()
-        
         self.paths = {} # self.paths maps parent nodes to a set-like list of valid neighbor nodes if their paths are connected internally by the intersection
-
-    def map_all_nodes(self):
-        """Creates an internal path between all parent and neighbor nodes"""
-        self.paths = {}
-        for parent in self.get_parents():
-            self.path[parent] = super().get_neighbors()
-
-    def map_nodes(self, parent: TrafficNode, neighbor: TrafficNode):
-        """Create an internal path from the input node `parent` to the output node `neighbor`"""
-        if neighbor in super().get_neighbors() and parent in self.get_parents():
-            if parent in self.paths.keys():
-                if neighbor not in self.paths[parent]: self.paths[parent].append(neighbor)
-            else:
-                self.paths[parent] = [neighbor]
 
     def get_neighbors(self, input_node):
         """Returns a list of nodes that are directly accessible from this node when coming from the node `input_node`"""
@@ -31,10 +16,28 @@ class Intersection(TrafficNode):
         """Returns a list of edges that are directly accessible from this node when coming from the node `input_node`"""
         return [self.neighbors[node] for node in self.paths[input_node]]
 
+    def map_all_nodes(self):
+        """Creates an internal path between all parent and neighbor nodes"""
+        self.paths = {}
+        for parent in self.get_parents():
+            self.path[parent] = super().get_neighbors()
 
-class LightIntersection(Intersection):
+    def map_nodes(self, parent: TrafficNode, neighbor: TrafficNode):
+        """Creates an internal path from the input node `parent` to the output node `neighbor`"""
+        if neighbor in super().get_neighbors() and parent in self.get_parents():
+            if parent in self.paths.keys():
+                if neighbor not in self.paths[parent]: self.paths[parent].append(neighbor)
+            else:
+                self.paths[parent] = [neighbor]
+
+
+class TrafficLight(Intersection):
 
     def __init__(self):
         super().__init__()
+        self.sequence = [[()],[]] # self.sequence is a list of lists of pairs that represent paths from (parent, neighbor)
 
+    def traverse(self, t):
+        #TODO: redefine
+        pass
     
--- roadmaps/model/network.py
+++ roadmaps/model/network.py
@@ -11,19 +11,6 @@ class TrafficNode(Node):
     
     def __init__(self):
         super().__init__()
-        self.parents = {} # self.neighbors is a dictionary mapping {parent: edge}
-
-    def get_parents(self):
-        """Returns a list of nodes that directly connect to this node"""
-        return list(self.parents.keys())
-
-    def get_input_edges(self):
-        """Returns a list of edges that directly connect to this node"""
-        return list(self.parents.values()) 
-
-    def connect_to(self, node, edge: Edge):
-        super().connect_to(node, edge)
-        node.parents[self] = edge
 
 
 class TrafficNetwork(Graph):
